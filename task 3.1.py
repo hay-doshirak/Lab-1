350e4cbbcd39a08713f18ec7b6b7dc012c0979f8
hay-doshirak <dashutkaab@gmail.com>
2025-03-01 15:54:26 +0300

    task1

---
+++ task 3.1.py
@@ -0,0 +1,29 @@
+print("Придумайте надежный пароль.(Длина пароля не менее 8 символов.)gg")
+password1=input("Ведите пароль: ")
+password2=input("Ведите пароль: ")
+
+s1="abcdefghijklmnopqrstuvwxyz"
+s2="0123456789"
+s3="!@#$%^&*(),.?\:{}|<>"
+
+def chek_pass(p):
+    if len(p)<8:
+        return "Ваш пароль слишком короткий."
+
+    if not any(x in p for x in s1):
+        return "Пароль должен содержать хотя бы одну строчную букву."
+
+    if not any(x in p for x in s1.upper()):
+        return "Пароль должен содержать хотя бы одну прописную букву."
+
+    if not any(x in p for x in s2):
+        return "Пароль должен содержать хотя бы одну цифру."
+
+    if not any(x in p for x in s3):
+        return "Пароль должен содержать хотя бы один спецсимвол."
+
+    else: return "Пароль принят."
+
+if password1==password2:
+    print(chek_pass(password1))
+else: print("Пароли не совпадают.")
