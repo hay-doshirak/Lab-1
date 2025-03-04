414ba3572ca6f175589b7023dacb6b5f12087c44
Dasha <dashutkaab@gmail.com>
2025-03-04 18:50:40 +0300

    Update task 3.3.py

--- task 3.3.py
+++ task 3.3.py
@@ -1,4 +1,6 @@
 year = int(input("Введите год: "))
-if (year>0) and((year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)):
-    print("Год", year, "- високосный.")
-else: print("Это год не високосный")
+if year>0 and year<=9999:
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)):
+        print("Год", year, "- високосный.")
+    else: print("Это год не високосный")
+else: print("Ошибка ввода")
